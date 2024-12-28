a903c4bebfe006ec159fbc1a8c992f5e9db0d1b2
Tushar-Khandelwal-2004 <khandelwal.tushar8527@gmail.com>
2024-12-28 18:15:16 +0530

    Made a dynamic AddPost Page like Linkedin

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState, useEffect } from "react";
-
+// import { useState , useEffect } from "react";
+import { useState } from "react";
+import { PostComponent } from "./post";
 function App() {
-  let [counterValue, setCounter] = useState(true);
-  useEffect(function () {
-    setInterval(function () {
-      setCounter(counterValue => !counterValue);
-    }, 5000);
-  }, []);
-  return <div>
-    {counterValue && <Counter></Counter>}
-  </div>
-}
-function Counter() {
-  // let count=0;
-  const [count, setCount] = useState(0);
-
-  useEffect(function () {
-    let clock=setInterval(IncreaseCount, 1000);
-    return function(){
-      clearInterval(clock);
-    }
-  }, []);
-
-  function IncreaseCount() {
-    setCount(count => count + 1);
+  const [posts, setposts] = useState([]);
+  const postComponents = posts.map(post => <PostComponent
+    name={post.name}
+    subtitle={post.subtitle}
+    time={post.time}
+    image={post.image}
+    description={post.description}
+  />)
+  function addPost() {
+    setposts([...posts, {
+      name: "Tushar",
+      subtitle: "2636 Followers",
+      time: "1h ago",
+      image: "https://wallpapers.com/images/high/robert-pattinson-as-the-batman-at23zambqyb1i5nj.webp",
+      description: "Use GoTo.now as your preferred URL shortener for better tracking and easy sharing! This asset is available in the following sizes:",
+    }])
   }
-
-
-  return <div>
-    <h1>{count}</h1>
-    <button onClick={IncreaseCount}>Increase Count!!</button>
-  </div>
+  return (
+    <div style={{ backgroundColor: "#dfe6e9", height: "100vh" }}>
+      <button onClick={addPost}>Add Post!</button>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div>
+          {postComponents}
+        </div>
+      </div>
+    </div>
+  )
 }
-export default App
+
+export default App;
